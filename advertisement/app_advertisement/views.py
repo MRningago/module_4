25f5e902c7423d32f2c528f97204662c1bd3d535
MRningago <am1n07@mail.ru>
2023-07-15 12:50:11 +0300

    ДЗ по занятию 5

--- advertisement/app_advertisement/views.py
+++ advertisement/app_advertisement/views.py
@@ -4,6 +4,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('''
-        <h1 style="background-color: yellow; text-align: center; border: solid; border-radius: 5px;">Все ок!</h1>
-    ''')
+    return render(request, 'index.html')
+
+
+def top_sellers(request):
+    return render(request, "top-sellers.html")
